8f61491f03c83a92a107f919a26dcd28b30f6d5b
Salsa Gal <fielddashos@gmail.com>
2022-05-27 02:31:27 +1000

    Update Graphics::new visibility

--- src/graphics.rs
+++ src/graphics.rs
@@ -10,7 +10,7 @@ pub struct Graphics {
 }
 
 impl Graphics {
-    pub async fn new(window: &Window) -> Self {
+    pub(crate) async fn new(window: &Window) -> Self {
         let size = window.inner_size();
 
         let instance = wgpu::Instance::new(wgpu::Backends::all());
