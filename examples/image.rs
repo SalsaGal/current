703cf3057bb8d428c4bc6f39b370f3bec5cf4eca
Salsa Gal <fielddashos@gmail.com>
2022-06-19 23:44:32 +1000

    Correct crash when using textures

--- examples/image.rs
+++ examples/image.rs
@@ -15,14 +15,14 @@ impl Game for Image {
     fn init(data: &mut GameData) -> Self {
         Self {
             rect: Sprite::new_texture_rect(data.graphics, 0).with_transform(Transform {
-                translation: (-0.5, 0.0, 0.0).into(),
-                scale: (0.5, 1.0).into(),
+                translation: (-160.0, 0.0, 0.0).into(),
+                scale: (320.0, 480.0).into(),
                 ..Default::default()
             }),
             test: Sprite::new_path_rect(data.graphics, "examples/test.png", Filter::Linear)
                 .with_transform(Transform {
-                    translation: (0.5, 0.0, 0.0).into(),
-                    scale: (0.5, 1.0).into(),
+                    translation: (160.0, 0.0, 0.0).into(),
+                    scale: (320.0, 480.0).into(),
                     ..Default::default()
                 }),
         }
